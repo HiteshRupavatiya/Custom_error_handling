3518e7a0fa0d1ee09b4134629d81dc7cf9c89fd9
hitesh <hiteshr@zignuts.com>
2023-03-20 17:18:06 +0530

    UserController added

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,9 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
+use App\Traits\ListingApiTrait;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class UserController extends Controller
 {
-    //
+    use ListingApiTrait;
+
+    public function list(Request $request)
+    {
+        $users = User::get();
+        $this->ListingValidation();
+        $searchableFields = ['name', 'email'];
+        $data = $this->filterSearchPagination($users, $searchableFields);
+        return ok('Users Fetched Successfully', $data);
+    }
 }
