644dfc3014bde160fbeebb62f841e5f22984b147
hitesh <hiteshr@zignuts.com>
2023-03-20 18:46:30 +0530

    get, update, delete method added

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -20,8 +20,48 @@ class UserController extends Controller
         $data = $this->filterSearchPagination($user, $searchableFields);
 
         return ok('Users Fetched Successfully', [
-            'users' => $data['query']->get(),
+            'users' => $data['query']->with('phones')->get(),
             'count' => $data['count']
         ]);
+    }
+
+    public function get($id)
+    {
+        $user = User::find($id);
+        if ($user) {
+            return ok('User Fetched Successfully', $user);
+        }
+        return error('User Not Found');
+    }
+
+    public function update(Request $request, $id)
+    {
+        $request->validate([
+            'name'  => 'required|alpha|min:5|max:30',
+            'email' => 'required|email|unique:users,email|max:40',
+        ]);
+
+        $user = User::find($id);
+
+        if ($user) {
+            $user->update($request->only(
+                [
+                    'name',
+                    'email',
+                ]
+            ));
+            return ok('User Updated Successfully');
+        }
+        return error('User Not Found');
+    }
+
+    public function delete($id)
+    {
+        $user = User::find($id);
+        if ($user) {
+            $user->delete();
+            return ok('User Deleted successfully');
+        }
+        return error('User Not Found');
     }
 }
