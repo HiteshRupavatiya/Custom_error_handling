359d47c71f1da486337c791543b4e42f91c5be41
hitesh <hiteshr@zignuts.com>
2023-03-20 18:48:23 +0530

    added fillable property for mass assignament and defined belongsTo realtionship to user

--- app/Models/Phone.php
+++ app/Models/Phone.php
@@ -8,4 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 class Phone extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'phone_type',
+        'phone_number',
+        'user_id'
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
